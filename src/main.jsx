da67ebb8e1a5bfb5b31c7d5e58d0338f52f8b7a3
sagardcruzeofficial-rgb <sagardcruze.official@gmail.com>
2026-09-25 23:03:50 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { StoreProvider } from './context/StoreContext.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <StoreProvider>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </StoreProvider>
   </React.StrictMode>,
 )
