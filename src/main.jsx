994387299a21c249499ed21c1a19bf4e34ac7072
sagardcruzeofficial-rgb <sagardcruze.official@gmail.com>
2026-09-25 11:34:28 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { StoreProvider } from './context/StoreContext.jsx'
 import './index.css'
+import { StoreProvider } from './context/StoreContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
